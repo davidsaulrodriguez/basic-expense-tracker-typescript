2d7d69e0e2072027878212b622ed0c9a3ea57656
David Rodriguez <david@enyutech.com>
2023-07-27 14:37:21 -0400

    add the ExpenseList component (for now; there's more to do)

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
-const App = () => {
-    return (
-        <>
-            <h1 className='text-center'>Hello World!</h1>
-        </>
-    )
-}
-export default App
+import ExpenseList from "./components/expense-tracker/components/ExpenseList.tsx";
+import { useState } from "react";
 
+const App = () => {
+  const [expenses, setExpenses] = useState([
+    { id: 1, description: "aaa", amount: 30, category: "Books" },
+    { id: 2, description: "bbb", amount: 8, category: "Books" },
+    { id: 3, description: "ccc", amount: 22, category: "Books" },
+    { id: 4, description: "ddd", amount: 17, category: "Books" },
+  ]);
+
+  if (expenses.length === 0) {
+    return <p>No expenses</p>;
+  }
+
+  return (
+    <>
+      <ExpenseList
+        expenses={expenses}
+        onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
+      />
+    </>
+  );
+};
+export default App;
